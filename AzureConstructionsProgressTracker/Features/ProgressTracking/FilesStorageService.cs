b2038553c1653cd6c4e53f53cee8097a3435d254
ptekla <ptekla>
2015-11-17 10:31:53 +0100

    Added sotrage implementation

--- AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
+++ AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
@@ -2,6 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
 
 namespace AzureConstructionsProgressTracker.Features.ProgressTracking
 {
@@ -19,6 +24,25 @@ namespace AzureConstructionsProgressTracker.Features.ProgressTracking
             // - upload blob
             // - return URL
             throw new NotImplementedException();
+
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference(ContainerName);
+
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+
+            blockBlob.UploadFromStream(file.InputStream);
+
+            return blockBlob.Uri.ToString();
         }
     }
 }
